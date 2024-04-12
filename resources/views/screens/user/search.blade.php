b891b4c4df7e0e9811f7d6c792cfea6e685895f9
H-LAP-00022\kiet.vh <kiet.vh@briswell-vn.com>
2024-04-12 16:58:49 +0700

    refs #126111 OrderTask Prepare source code - Fix layout table result search

--- resources/views/screens/user/search.blade.php
+++ resources/views/screens/user/search.blade.php
@@ -12,7 +12,8 @@
                 <h2>
                     User Search
                 </h2>
-                <button class="btn btn-primary" type="button" data-add-route="{{route('admin.user.add')}}" name="addButton" id="addButton">Add User</button>
+                <button class="btn btn-primary" type="button" data-add-route="{{ route('admin.user.add') }}"
+                    name="addButton" id="addButton">Add User</button>
             </div>
             <form name="formSearch" id="formSearch" method="POST" action="{{ route('admin.user.handleSearch') }}">
                 @csrf
@@ -48,28 +49,30 @@
                 </div>
 
                 <div class="d-flex justify-content-end m-3 ">
-                    <x-button.base label="Search" class="btn btn-secondary m-1" ></x-button.base>
+                    <x-button.base label="Search" class="btn btn-secondary m-1"></x-button.base>
                     <x-button.clear label="Clear" screen="user.search" class="btn btn-secondary m-1"></x-button.clear>
-                    <x-button.base  type="submit" name="btnExport" label="Export" screen="usr01" formaction="{{ route('admin.user.search') }}" class="btn btn-secondary m-1"></x-button.base>
-                    <x-button.base type="button" label="Import" class="btn btn-secondary m-1" data-bs-toggle="modal" data-bs-target="#importCSVModal"></x-button.base>
+                    <x-button.base type="submit" name="btnExport" label="Export" screen="usr01"
+                        formaction="{{ route('admin.user.search') }}" class="btn btn-secondary m-1"></x-button.base>
+                    <x-button.base type="button" label="Import" class="btn btn-secondary m-1" data-bs-toggle="modal"
+                        data-bs-target="#importCSVModal"></x-button.base>
                 </div>
             </form>
         </div>
     </div>
-        <div class="card card-secondary card-outline">
-            <div class="card-body">
-                <div class="col" id="table_data">
-                    @if (count($users) > 0)
-                        <div class="card">
-                            <div class="card-header">
-                                <div class="d-flex row justify-content-between align-item-center">              
-                                   {{ $users->links('common.pagination') }}
-                                </div>
+    <div class="card card-secondary card-outline">
+        <div class="card-body">
+            <div class="col" id="table_data">
+                @if (count($users) > 0)
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="d-flex row justify-content-between align-item-center">
+                                {{ $users->links('common.pagination') }}
                             </div>
+                        </div>
 
-                            <div class="card-body p-0">
-                                <table class="table table-bordered table-responsive-md">
-                                    <thead>
+                        <div class="card-body p-0">
+                            <table class="table table-bordered table-responsive-md">
+                                <thead>
                                     <tr>
                                         <th></th>
                                         <th>Email</th>
@@ -79,79 +82,83 @@
                                         <th>Phone</th>
                                         <th>Address</th>
                                     </tr>
-                                    </thead>
-                                    <tbody>
+                                </thead>
+                                <tbody>
 
                                     @foreach ($users as $user)
                                         <tr id="userRow_{{ $user->id }}">
-                                            <td class="d-flex align-items-center justify-content-center">
-                                                <a href="{{ route('admin.user.edit', ['id' => $user->id]) }}"
-                                                   class="btn btn-primary mr-2 edit-btn">Edit</a>
+                                            <td style="width: 5%;" class="d-table-cell align-middle">
+                                                <div class="d-flex justify-content-center">
+                                                    <a href="{{ route('admin.user.edit', ['id' => $user->id]) }}"
+                                                        class="btn btn-primary mr-2 edit-btn">Edit</a>
 
-                                                <form name="deleleUser" id="deleteForm_{{ $user->id }}"
-                                                      data-add-route="{{ route('admin.user.delete', ['id' => $user->id]) }}"
-                                                      action="{{ route('admin.user.delete', ['id' => $user->id]) }}" method="GET"
-                                                      class="delete-user">
+                                                    <form name="deleleUser" id="deleteForm_{{ $user->id }}"
+                                                        data-add-route="{{ route('admin.user.delete', ['id' => $user->id]) }}"
+                                                        action="{{ route('admin.user.delete', ['id' => $user->id]) }}"
+                                                        method="GET" class="delete-user">
 
-                                                    <button type="submit" class="btn btn-danger delete-btn">Delete</button>
+                                                        <button type="submit"
+                                                            class="btn btn-danger delete-btn">Delete</button>
 
-                                                </form>
+                                                    </form>
+                                                </div>
 
                                             </td>
-                                            <td>{{ $user->email }}</td>
-                                            <td>{{ $user->name }}</td>
-                                            <td>{{ $user->user_flg == 0 ? 'Admin' : ($user->user_flg == 2 ? 'Support' : 'User') }}
+                                            <td style="width: 20%;" class="text-break">{{ $user->email }}</td>
+                                            <td style="width: 25%;"  class="text-break">{{ $user->name }}</td>
+                                            <td style="width: 10%;" class="text-break">{{ $user->user_flg == 0 ? 'Admin' : ($user->user_flg == 2 ? 'Support' : 'User') }}
                                             </td>
-                                            <td>
+                                            </td>
+                                            <td style="width: 15%;" class="text-break">
                                                 @if ($user->date_of_birth)
                                                     {{ $user->date_of_birth->format('d/m/Y') }}
                                                 @endif
                                             </td>
-                                            <td>{{ $user->phone }}</td>
-                                            <td>{{ $user->address }}</td>
+                                            <td style="width: 15%;" class="text-break">{{ $user->phone }}</td>
+                                            <td style="width: 25%;" class="text-break">{{nl2br($user->address)}}</td>
                                         </tr>
                                     @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
+                                </tbody>
+                            </table>
                         </div>
-                    @else
-                        <div class="alert alert-info" role="alert">
-                            <p class="m-0 p-0">There is no result.</p>
-                        </div>
-                    @endif
-                </div>
-            </div>
-
-        </div>
-          <!-- Modal -->
-          <div class="modal fade" id="importCSVModal" tabindex="-1" aria-labelledby="importCSVModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="importCSVModalLabel">Import CSV</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                    <form action="{{route('admin.user.import')}}" method="post" enctype="multipart/form-data"
-                          name="formImportCSV" id="formImportCSV">
-                        @csrf
-                        <div class="modal-body">
-
-                            <div class="mb-3">
-                                <label for="csvFile" class="form-label">Choose your CSV File</label>
-                                <input class="form-control" type="file" id="csvFile" name="csvFile">
-                            </div>
-
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="submit" class="btn btn-primary">Import</button>
-                        </div>
-                    </form>
-                </div>
+                @else
+                    <div class="alert alert-info" role="alert">
+                        <p class="m-0 p-0">There is no result.</p>
+                    </div>
+                @endif
             </div>
         </div>
-        @push('scripts')
-            @vite(['resources/js/screens/user/search.js'], 'build')
-        @endpush
+
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="importCSVModal" tabindex="-1" aria-labelledby="importCSVModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="importCSVModalLabel">Import CSV</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form action="{{ route('admin.user.import') }}" method="post" enctype="multipart/form-data"
+                    name="formImportCSV" id="formImportCSV">
+                    @csrf
+                    <div class="modal-body">
+
+                        <div class="mb-3">
+                            <label for="csvFile" class="form-label">Choose your CSV File</label>
+                            <input class="form-control" type="file" id="csvFile" name="csvFile">
+                        </div>
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Import</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    @push('scripts')
+        @vite(['resources/js/screens/user/search.js'], 'build')
+    @endpush
 </x-app-layout>
